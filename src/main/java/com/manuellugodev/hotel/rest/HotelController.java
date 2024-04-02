bbd7a20ed3fbf683b4310aaa5f5871fb8ef16246
Manuel Lugo <manuellugo2000ml@gmail.com>
2024-04-02 17:43:50 -0500

    Implemented getRoomAvailable by Date

--- src/main/java/com/manuellugodev/hotel/rest/HotelController.java
+++ src/main/java/com/manuellugodev/hotel/rest/HotelController.java
@@ -59,8 +59,8 @@ public class HotelController {
     @GetMapping("/rooms")
     public ResponseEntity<List<Room>> getRooms(
             @RequestParam Boolean available,
-            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date dStartTime,
-            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date dEndTime) {
+            @RequestParam Date dStartTime,
+            @RequestParam Date dEndTime) {
 
         try {
             return ResponseEntity.ok(roomService.getRoomsAvailable(dStartTime,dEndTime));
